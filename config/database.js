ff6ace62797a0a2d44f77731f467cb78d55323eb
unknown <rafliedka33@gmail.com>
2022-06-10 16:00:06 +0100

    add env file

--- config/database.js
+++ config/database.js
@@ -10,10 +10,17 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
   },
   test: {
     username: DB_USER,
